7e8d2a6c2b8229ee2cd26c10925ac79e58df3bac
victoria6991 <miss.victoria-liar@yandex.ru>
2022-03-26 00:54:48 +0300

    all test failed

--- src/main/java/ru/netology/service/CashbackHackService.java
+++ src/main/java/ru/netology/service/CashbackHackService.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
 public class CashbackHackService {
-    private final int boundary = 1000;
+    private final int boundary = 10000;
 
     public int remain(int amount) {
         return boundary - amount % boundary;
